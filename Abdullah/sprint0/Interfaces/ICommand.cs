7156a622313963009f7c9a8f8f18db78ef6b3516
Abdullah Felemban <149270528+Abadi2225@users.noreply.github.com>
2026-02-16 16:35:39 -0500

    Delete Abdullah/sprint0 directory
    To Match the main branch

--- Abdullah/sprint0/Interfaces/ICommand.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Xna.Framework;
-
-namespace sprint0.Interfaces
-{
-    public interface ICommand
-    {
-        void Execute(Game1 game1);
-    }
-}
--- Abdullah/sprint0/Interfaces/IController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Xna.Framework;
-
-namespace sprint0.Interfaces
-{
-    public interface IController
-    {
-        void Update(GameTime gameTime, Game1 game);
-    }
-}
