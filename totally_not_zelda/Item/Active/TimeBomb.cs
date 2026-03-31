3120d5ffac4f782da58c987a6dac112d504d0cf5
leaf09986 <leaf09986@gmail.com>
2026-03-30 20:49:27 -0400

    add sound player and added sounds to some events

--- totally_not_zelda/Item/Active/TimeBomb.cs
+++ totally_not_zelda/Item/Active/TimeBomb.cs
@@ -1,5 +1,6 @@
 using Microsoft.Xna.Framework;
 
+using Sprint.Sound;
 using Sprint.Sprites;
 
 namespace Sprint.Item;
@@ -7,8 +8,7 @@ namespace Sprint.Item;
 internal class TimeBomb : AbstractItem
 {
     private double millisUntilExplode;
-
-    public bool IsExploding { get; private set; }
+    private bool exploded = false;
 
     public TimeBomb(double explodeDelayMillis, string name, Vector2 pos, Rectangle sourceRect, float scale) : base(name, GameServices.ItemSheet, pos)
     {
@@ -19,16 +19,14 @@ internal class TimeBomb : AbstractItem
 
     public override void Update(GameTime time)
     {
-        if (IsExploding)
-        {
-            IsExploding = false;
-            return;
-        }
-
         millisUntilExplode -= time.ElapsedGameTime.TotalMilliseconds;
-        if (millisUntilExplode <= 0)
-            IsExploding = true;
+        if (millisUntilExplode <= 0 && !exploded)
+        {
+            exploded = true;
+            // run explode triggers
+            SoundPlayer.Play(SoundType.BOMB_EXPLODE);
+        }
     }
 
-    public override bool IsFinished => millisUntilExplode <= 0 && !IsExploding;
+    public override bool IsFinished => millisUntilExplode <= 0 && exploded;
 }
---
+++ totally_not_zelda/Sound/SoundPlayer.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Sprint.Sound;
+
+internal class SoundPlayer
+{
+    private static Dictionary<SoundType, string> soundLookup = new Dictionary<SoundType, string>()
+    {
+        { SoundType.ARROW_BOOMERANG, "sounds/LOZ_Arrow_Boomerang" },
+        { SoundType.BOMB_EXPLODE, "sounds/LOZ_Bomb_Blow" },
+        { SoundType.BOMB_PLACE, "sounds/LOZ_Bomb_Drop" },
+        { SoundType.BOSS_HURT, "sounds/LOZ_Boss_Hit" },
+        { SoundType.BOSS_SCREAM1, "sounds/LOZ_Boss_Scream1" },
+        { SoundType.BOSS_SCREAM2, "sounds/LOZ_Boss_Scream2" },
+        { SoundType.BOSS_SCREAM3, "sounds/LOZ_Boss_Scream3" },
+        { SoundType.CANDLE, "sounds/LOZ_Candle" },
+        { SoundType.DOOR_UNLOCK, "sounds/LOZ_Door_Unlock" },
+        { SoundType.ENEMY_DEATH, "sounds/LOZ_Enemy_Die" },
+        { SoundType.ENEMY_HURT, "sounds/LOZ_Enemy_Hit" },
+
+        { SoundType.PICKUP_VALUABLE, "sounds/LOZ_Fanfare" },
+        { SoundType.LINK_HEALED, "sounds/LOZ_Get_Heart" },
+        { SoundType.PICKUP_ITEM, "sounds/LOZ_Get_Item" },
+        { SoundType.PICKUP_RUPEE, "sounds/LOZ_Get_Rupee" },
+        { SoundType.KEY_APPEAR, "sounds/LOZ_Key_Appear" },
+
+        { SoundType.LINK_DEATH, "sounds/LOZ_Link_Die" },
+        { SoundType.LINK_HURT, "sounds/LOZ_Link_Hurt" },
+        { SoundType.LOW_HEALTH, "sounds/LOZ_LowHealth" },
+
+        { SoundType.SECRET_UNLOCKED, "sounds/LOZ_Secret" },
+
+        { SoundType.SWORD_COMBINED, "sounds/LOZ_Sword_Combined" },
+        { SoundType.SWORD_SHOOT, "sounds/LOZ_Sword_Shoot" },
+        { SoundType.SWORD_SWING, "sounds/LOZ_Sword_Slash" }
+    };
+
+    public static void Play(SoundType type)
+    {
+        string filename = soundLookup[type];
+        if (filename == null) return;
+
+        Game1.Instance.Content.Load<SoundEffect>(filename).Play();
+    }
+}
